80498319dccaab093af19a1f558cb36157588c58
Ryan Pitman <42426322+boarrr@users.noreply.github.com>
2021-07-31 17:24:25 +0100

    Solution to problem 1 of Project Euler:
    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
    
    Find the sum of all the multiples of 3 or 5 below 1000.
    
    Answer: 233168

---
+++ Project Euler/Problem1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Project_Euler
+{
+    public class Problem1 
+    {
+        public int Num { get; init; }
+        private int Total { get; set; }
+        
+        public int SumMult()
+        {
+            Total = 0;
+            for (var i = 0; i < Num; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    Total += i;
+                }
+            }
+            return Total;
+        }
+    }
+
+    internal static class Program
+    {
+        public static void Main()
+        {
+            var testCase = new Problem1 {Num = 1000};
+            
+            Console.WriteLine(testCase.SumMult());
+        }
+    }
+}
+
