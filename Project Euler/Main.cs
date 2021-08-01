12653b3da5e01ac5149278e8fb53e26d4e0503b8
Ryan Pitman <42426322+boarrr@users.noreply.github.com>
2021-08-01 06:24:50 +0100

    Problem 5 Completed

--- Project Euler/Main.cs
+++ Project Euler/Main.cs
@@ -16,12 +16,14 @@ namespace Project_Euler
             var problem1 = new Problem1 {Num = 1000};
             var problem2 = new Problem2 {Nth = 4000000};
             var problem3 = new Problem3 {Num = 600851475143};
+            var problem5 = new Problem5();
 
             // Log our solutions
             Console.WriteLine("Answer to Problem 1: {0}", problem1.SumMult());
             Console.WriteLine("Answer to Problem 2: {0}", problem2.Fibonacci());
             Console.WriteLine("Answer to Problem 3: {0}", problem3.PrimeFac());
             Console.WriteLine("Answer to Problem 4: {0}", Problem4.Palindrome());
+            Console.WriteLine("Answer to Problem 5: {0}", Problem5.RemainderFunc());
         }
     }
 }
